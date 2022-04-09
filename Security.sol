e90f87a6e52815417777c75918900f05b7624002
Dylan Melotik <60583747+dmelotik@users.noreply.github.com>
2022-04-09 16:34:21 -0500

    create security.sol

---
+++ Security.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.13;
+
+/**
+ * @title Solidity Security Issues
+ * @notice Example contract intended for learning
+ * @author Dylan & Jared
+ */
+contract Security {
+
+  // TODO: write main security hacks and how to prevent
+
+}
